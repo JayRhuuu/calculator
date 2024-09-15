8ec293a06529a1d9c0c4ce8716cd13c1605dcf63
RhuRhuuuu <94213547+JayRhuuu@users.noreply.github.com>
2024-09-16 00:48:42 +0800

    incomplete

--- script.js
+++ script.js
@@ -1,76 +1,75 @@
 function addButtonClickListener(buttonId, displayId) {
-    document.getElementById(buttonId).addEventListener("click", function () {
-      const btn = document.getElementById(buttonId).innerHTML;
-      document.getElementById(displayId).value += btn;
-  
-      console.log(document.getElementById("display").value);
-    });
-  }
-  
-  function clearOnce() {
-    document.getElementById("clearOnce").addEventListener("click", function () {
-      document.getElementById("display").value = document
-        .getElementById("display")
-        .value.slice(0, document.getElementById("display").value.length - 1);
-  
-      console.log(document.getElementById("display").value);
-    });
-  }
-  
-  function clearAll() {
-    document.getElementById("c").addEventListener("click", function () {
-      document.getElementById("display").value = "";
-  
-      console.log("clear all");
-    });
-  }
-  
-  function displayResult() {
-    document.getElementById("equal").addEventListener("click", function () {
-      try {
-        document.getElementById("display").value = eval(
-          document.getElementById("display").value
-        ).toString();
-  
-        if (0 == document.getElementById("display").value){
-          document.getElementById("display").value = "";
-        }
-      } catch (error) {
-        document.getElementById("display").value = "Invalid Syntax";
+  document.getElementById(buttonId).addEventListener("click", function () {
+    const btn = document.getElementById(buttonId).innerHTML;
+    document.getElementById(displayId).value += btn;
+
+    console.log(document.getElementById("display").value);
+  });
+}
+
+function clearOnce() {
+  document.getElementById("clearOnce").addEventListener("click", function () {
+    document.getElementById("display").value = document
+      .getElementById("display")
+      .value.slice(0, document.getElementById("display").value.length - 1);
+
+    console.log(document.getElementById("display").value);
+  });
+}
+
+function clearAll() {
+  document.getElementById("c").addEventListener("click", function () {
+    document.getElementById("display").value = "";
+
+    console.log("clear all");
+  });
+}
+
+function displayResult() {
+  document.getElementById("equal").addEventListener("click", function () {
+    try {
+      document.getElementById("display").value = eval(
+        document.getElementById("display").value
+      ).toString();
+
+      if (0 == document.getElementById("display").value){
+        document.getElementById("display").value = "";
       }
-    });
-  }
-  
-  function attachListeners() {
-    const classes = [
-      "one",
-      "two",
-      "three",
-      "four",
-      "five",
-      "six",
-      "seven",
-      "eight",
-      "nine",
-      "zero",
-      "doubleZero",
-      "dot",
-      "divide",
-      "times",
-      "minus",
-      "plus",
-    ];
-    // classes.forEach(e=>{
-    //   addButtonClickListener(e, "display");
-    // })
-  
-    for (let i = 0; i < classes.length; i++) {
-      addButtonClickListener(classes[i], "display");
+    } catch (error) {
+      document.getElementById("display").value = "Invalid Syntax";
     }
+  });
+}
+
+function attachListeners() {
+  const classes = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "zero",
+    "doubleZero",
+    "dot",
+    "divide",
+    "times",
+    "minus",
+    "plus",
+  ];
+  // classes.forEach(e=>{
+  //   addButtonClickListener(e, "display");
+  // })
+
+  for (let i = 0; i < classes.length; i++) {
+    addButtonClickListener(classes[i], "display");
   }
-  
-  displayResult();
-  clearAll();
-  clearOnce();
-  attachListeners();
-  
+}
+
+displayResult();
+clearAll();
+clearOnce();
+attachListeners();
